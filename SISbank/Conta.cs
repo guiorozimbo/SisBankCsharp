f0233b8b87519d8daa47cf4cc5b15a05b95eb2f0
Guilherme <guilhermegv890@gmail.com>
2023-02-10 00:00:51 -0300

    SISBANK

---
+++ SISbank/Conta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisBank
+{
+    public class Conta
+    {
+        public int agencia;
+        public string conta;
+        public string nome;
+        public double saldo;
+        public int cpf;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            this.saldo -= valor;
+        }
+    }
+}
---
+++ SISbank/Program.cs
@@ -0,0 +1,15 @@
+﻿using SisBank;
+
+Conta contaGuilherme = new Conta();
+contaGuilherme.nome = "Guilherme Ramos";
+contaGuilherme.agencia = 4002;
+contaGuilherme.conta = "89225851-6";
+contaGuilherme.saldo = 10000;
+contaGuilherme.cpf = 1125354785;
+Console.WriteLine("Saldo = " + contaGuilherme.saldo);
+
+contaGuilherme.Depositar(500);
+Console.WriteLine("Saldo após o deposito = " + contaGuilherme.saldo);
+
+contaGuilherme.Sacar(250);
+Console.WriteLine("Saldo após o saque = " + contaGuilherme.saldo);
